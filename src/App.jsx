789c7a73581ec39a7b925d952f91f95efefc7e7d
nikhilcse0427 <nikhilcse0427@gmail.com>
2025-05-03 19:54:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <center>
+      <p className='text-3xl text-red-700 font-bold'>Welcome to LinkShrink - <span className='text-blue-600'>urlShortner</span></p>
+    </center>
+    </>
+  )
+}
+
+export default App
